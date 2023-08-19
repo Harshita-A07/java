e044d6d7618ccc61091b0baa1390daeaf61f88b8
Harshita-A07 <121336740+Harshita-A07@users.noreply.github.com>
2023-08-19 21:37:25 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,22 @@
+//5 pattern 
+
+package java1;
+import java.util.*;
+public class Java1
+{
+    public static void main(String[] args)
+    {
+        int n,j;
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter range n:");
+        n=obj.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+        System.out.println("");
+        }
+    }
+} 
